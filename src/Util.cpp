e9a0ce00a19c2b9e450be087fd963b0a25181e62
guruhegde <s8guhegd.stud.uni-saarland.de>
2019-02-12 10:53:01 +0100

    Implement variable annotation detection mechnanism
    This commit includes a function which takes Function object in IR form
    Method detects annotated variable and it's use list in in IR instructions
    for the input Function.

--- src/Util.cpp
+++ src/Util.cpp
@@ -1,12 +1,19 @@
+#include <llvm/ADT/SmallVector.h>
 #include <llvm/IR/Function.h>
+#include <llvm/IR/Instruction.h>
 #include <llvm/IR/Instructions.h>
+#include <llvm/IR/InstIterator.h>
 #include <llvm/IR/Module.h>
+#include <llvm/Support/Debug.h>
 
 #include "Util.h"
+
 
 using namespace llvm;
 
 namespace tas {
+
+Intrinsic::ID VarAnnotationId = static_cast<Intrinsic::ID>(177);
 
 void setAnnotationInFunctionObject(Module * M) {
   if (auto annotationList = M->getNamedGlobal("llvm.global.annotations")) {
@@ -18,6 +25,24 @@ void setAnnotationInFunctionObject(Module * M) {
             cast<GlobalVariable>(ca_struct->getOperand(1)->getOperand(0))->getOperand(0));
 
         ca_func->addFnAttr(ca_annotation->getAsCString());
+      }
+    }
+  }
+}
+
+  void detectVarAnnotation(Function * F) {
+  SmallVector<Instruction *, 8> ExpPtrUseList;
+  for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+    if (auto * CI = dyn_cast<CallInst>(&*I)) {
+      if (CI->getCalledFunction()->getIntrinsicID() == VarAnnotationId) {
+        LLVM_DEBUG(dbgs() << "Use of annotated ptr in load instruction\n");
+        auto * ExprPtr = (cast<BitCastInst>(CI->getOperand(0)))->getOperand(0);
+        for (User * U : ExprPtr->users()) {
+          if (auto * Inst = dyn_cast<LoadInst>(U)) {
+            LLVM_DEBUG(dbgs() << *Inst << "\n");
+            ExpPtrUseList.push_back(Inst);
+          }
+        }
       }
     }
   }
--- src/Util.h
+++ src/Util.h
@@ -2,6 +2,9 @@
 #define TAS_UTIL_H
 
 #include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+
+#define DEBUG_TYPE "tas"
 
 namespace tas {
 
@@ -11,6 +14,10 @@ namespace tas {
 // of in-memory IR. This facilitates applying function level transformation to the function
 // of interest to us.
 void setAnnotationInFunctionObject(llvm::Module * M);
+
+// This function identifies pointer variables that are marked as expensive memory load
+// operation. 
+void detectVarAnnotation(llvm::Function * F);
 
 } // namespace tas
 
