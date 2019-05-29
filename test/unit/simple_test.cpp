f3cc4ddad7933737dfe67236606176569878b51d
guruhegde <s8guhegd.stud.uni-saarland.de>
2019-05-29 15:36:36 +0200

    Test array creation and access

--- test/unit/simple_test.cpp
+++ test/unit/simple_test.cpp
@@ -5,7 +5,9 @@
 #include <llvm/IR/CFG.h>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/Instruction.h>
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Module.h>
+#include "llvm/IR/Verifier.h"              /* verifyFunction, verifyModule */
 #include <llvm/IRReader/IRReader.h>
 #include <llvm/Support/raw_ostream.h>
 #include "llvm/Support/SourceMgr.h"
@@ -17,18 +19,36 @@ using namespace llvm;
 namespace tas {
 
 TEST_CASE("Read IR file") {
-
   LLVMContext Context;
   SMDiagnostic Err;
-  std::unique_ptr<Module> M (parseIRFile("output.ll",  Err, Context));
+  std::unique_ptr<Module> M (parseIRFile("array_create.ll",  Err, Context));
 
   REQUIRE (M.get() != nullptr);
-  auto * F = M->getFunction("fast_flows_packet_fss");
+  auto * F = M->getFunction("main");
+  errs() << F->getName() << ":\n";
   
+
+  IRBuilder<> Builder (Context);
   auto BB = F->begin();
-  ++BB;
-  auto * TL0 = TASForLoop::Create(F->getContext(), &F->getEntryBlock(), &*BB, "tas.loop.0", F);
+  auto TermI = (*BB).getTerminator();
+
+  Builder.SetInsertPoint(TermI);
+  auto arrayPtr = Builder.CreateAlloca(ArrayType::get(Builder.getInt32Ty(), 32));
+
+  ++BB; ++BB;
+  TermI = (*BB).getTerminator();
+  //auto zero = ConstantInt::get(Context, APInt(64, 0, true));
+
+  Builder.SetInsertPoint(TermI);
+  auto index = ConstantInt::get(Context, APInt(32, 0, true));
+  auto ptr = Builder.CreateGEP(arrayPtr, {Builder.getInt64(0), index}, "index");
+  auto store = Builder.CreateStore(index, ptr);
+  auto load = Builder.CreateLoad(ptr);
+
   F->print(errs());
+
+  verifyModule(*M);
+  M->dump();
 }
 
 }
