3a3dc192fb7330448b73e0843beb3b6b78bb1b3f
guruhegde <s8guhegd.stud.uni-saarland.de>
2019-05-29 23:15:21 +0200

    Update test run script

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -eu
 
+make
 # Run using debug build
 LLVM_PATH=/home/gp/llvm_projects/llvm/install/bin
 
 # update input here
-INPUT=lookup.c
+#INPUT=lookup.c
 
-#${LLVM_PATH}/clang -S -emit-llvm ../examples/$INPUT -ooutput.ll
+#${LLVM_PATH}/clang -S -emit-llvm examples/$INPUT -oexamples/output.ll
 #opt -load lib/libTASPrototypePasses.so < examples/lookup.ll >/dev/null -tas-loop-details
-${LLVM_PATH}/opt -S -load lib/libTASPass.so <../examples/fast_flows_O0.ll > output.ll -tas-batch-process -debug-only=tas
+${LLVM_PATH}/opt -S -load lib/libTASPass.so <../examples/fast_flows.ll > output.ll -mem2reg -indvars -loop-simplify -lcssa -tas-batch-process -debug-only=tas
