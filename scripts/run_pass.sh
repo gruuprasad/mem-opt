4a87c62acb8d2d2b96d3049a7f9bca833da2d026
guruhegde <s8guhegd.stud.uni-saarland.de>
2019-06-13 01:56:59 +0200

    Update run_pass script
    Fixes #4

--- scripts/run_pass.sh
+++ scripts/run_pass.sh
@@ -9,7 +9,15 @@ LLVM_PATH=/home/gp/llvm_projects/llvm/install/bin
 INPUT=batch_process_test1.c
 
 #${LLVM_PATH}/clang -S -emit-llvm $INPUT -ooutput.ll
-#opt -load lib/libTASPrototypePasses.so < examples/lookup.ll >/dev/null -tas-loop-details
-${LLVM_PATH}/opt -S -load lib/libTASPass.so <output.ll > output_tas.ll -mem2reg -indvars -loop-simplify -tas-batch-process -debug-only=tas
-${LLVM_PATH}/llc -filetype=obj output_tas.ll -o batch_process_test1.o
+${LLVM_PATH}/opt -S -load lib/libTASPass.so <fast_flows.ll > fast_flows_tas.ll -mem2reg -indvars -loop-simplify -tas-batch-process
+${LLVM_PATH}/llc -filetype=obj fast_flows_tas.ll -o fast_flows.o
+${LLVM_PATH}/llc fast_flows_tas.ll -o fast_flows.s
+
+#${LLVM_PATH}/clang -S -emit-llvm -c batch_process_test1.c
+#${LLVM_PATH}/opt -S -load lib/libTASPass.so <batch_process_test1.ll > batch_process_test1_1.ll -tas-batch-process
+#clang -Xclang -load -Xclang lib/libTASPass.so -O0 -c batch_process_test1.c
+#${LLVM_PATH}/clang -Xclang -load -Xclang lib/libTASPass.so -O0 -S -emit-llvm batch_process_test1.c
+
+${LLVM_PATH}/opt -S -load lib/libTASPass.so <batch_process_test1.ll > batch_process_test1_tas.ll -mem2reg -indvars -loop-simplify -lcssa
+${LLVM_PATH}/llc -filetype=obj batch_process_test1_tas.ll -o batch_process_test1.o
 ${LLVM_PATH}/clang batch_process_test1.o -o batch_process_test1
