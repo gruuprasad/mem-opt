8e11c3674d4e52589f2d491419726d6a5616da30
guruhegde <s8guhegd.stud.uni-saarland.de>
2019-06-18 10:31:48 +0200

    Briefly summarize pass outline

--- src/BatchMaker.cpp
+++ src/BatchMaker.cpp
@@ -18,6 +18,21 @@ namespace tas {
 
 bool BatchMaker::run() {
 
+/* Steps:
+ *
+ * 1. Identify batch and non-batch related arguments in scalar function (non-batch)
+ * 2. Create batch function with appropriate input parameter types.
+ * 3. Create Entry basic block [ENTRYBB].
+ * 4. Move instructions with non-batch related arguments to entry basic block.
+ * 5. Create basic block with set up related to batched argument, followed by entry basic block [BATCH_ENTRYBB].
+ * 6. Add empty for loop followed by BATCH_ENTRYBB.
+ * 7. Determine which basic blocks in scalar function is going to be part of the for loop body.
+ * 8. Clone basic block into batch function.
+ * 9. Replace scalar value uses with batched arguments.
+ *    NOTE: First we form single loop, hence intermeditate values need not be batched.
+ * 10. Move any remaining basic block followed by for loop if exists.
+ */
+
   createBatchedFormFn();
   OldFunc->getParent()->dump();
 
