8cd30ecc516c9ba2f48dbb14a088021b7142edce
guruhegde <s8guhegd.stud.uni-saarland.de>
2019-07-30 10:20:30 +0200

    [Class rename] CacheUsageAnalysis -> CacheUsageInfo

---
+++ include/CacheUsageInfo.h
@@ -0,0 +1,61 @@
+#ifndef TAS_CACHEUSAGEANALYSIS_HPP
+#define TAS_CACHEUSAGEANALYSIS_HPP
+
+#include <string>
+
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/Analysis/LoopInfo.h>
+#include <llvm/Analysis/MemorySSA.h>
+#include <llvm/IR/Dominators.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Value.h>
+
+namespace tas {
+
+/// This holds the result of cache analysis performed on a function.
+/// Currently the only result data we have is the usage of number of
+/// cache lines.
+/// FIXME How to properly use this kind of result data along with
+/// pass manager?
+struct CAResult {
+  unsigned NumOfCacheLines; //< Estimated number of cache lines.
+  CAResult() = default;
+  CAResult(unsigned N) : NumOfCacheLines(N) {}
+};
+
+llvm::raw_ostream &operator<<(llvm::raw_ostream &OS, CAResult AR);
+
+/// \param[out] NumOfCacheLines Result of the cache analysis
+class CacheUsageInfo {
+  llvm::Function * F;
+  const llvm::DataLayout * DL;
+  llvm::DominatorTree DT;
+  llvm::LoopInfo LI;
+  llvm::SmallVector<const llvm::AllocaInst *, 4> PtrAllocas;
+  unsigned CacheLineSize;
+  unsigned NumOfCacheLines = 0;
+  bool LoopExists = false;
+
+  public:
+  CacheUsageInfo(llvm::Function * F, unsigned N = 64)
+    : F(F), DL(&F->getParent()->getDataLayout()),
+      DT(llvm::DominatorTree(*F)), LI(llvm::LoopInfo(DT)),
+      CacheLineSize(N) {}
+
+  bool analyze();
+  unsigned getNumOfCacheLines() {
+    return NumOfCacheLines;
+  }
+
+  /// Hand off cache analysis result
+  CAResult getResult() {
+    return CAResult(NumOfCacheLines);
+  }
+
+  unsigned getByteOffsetRelative(llvm::Type *, unsigned FieldIdx);
+  unsigned getByteOffsetAbsolute(const llvm::GetElementPtrInst * BasePtr, unsigned CurOffset);
+}; // tas namespace
+
+}
+
+#endif
---
+++ src/CacheAnalysis/CacheUsageInfo.cpp
@@ -0,0 +1,118 @@
+#include "CacheUsageInfo.h"
+#include "Macros.h"
+#include "Util.h"
+
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/ADT/Statistic.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Support/Debug.h>
+
+#include <algorithm>
+#include <string>
+
+using namespace llvm;
+
+#define DEBUG_TYPE "tas"
+
+namespace tas {
+
+raw_ostream &operator<<(raw_ostream &OS, CAResult AR) {
+  OS << "Estimated number of cache lines = " << AR.NumOfCacheLines << "\n";
+  return OS;
+}
+
+unsigned CacheUsageInfo::getByteOffsetRelative(Type * Ty, unsigned FieldIdx) {
+  if (Ty->isSingleValueType())
+    return 0;
+
+  // Struct type
+  if (Ty->isStructTy()) {
+    //errs() << "In getByteOffsetRelative() struct name = " << Ty->getStructName() << "\n";
+    auto * TyLayout = DL->getStructLayout(cast<StructType>(Ty));
+    return TyLayout->getElementOffset(FieldIdx);
+  }
+
+  // Array type
+  return DL->getTypeAllocSize(cast<ArrayType>(Ty)->getArrayElementType()) * FieldIdx;
+}
+
+/* This method recursively computes the byte offset of the field to be accessed.
+ * In a sequence of GEP instruction, for each instruction relative offset is
+ * computed and accumulated.
+ */
+unsigned CacheUsageInfo::getByteOffsetAbsolute(const GetElementPtrInst * CurGEP, unsigned CurOffset) {
+  // XXX Assume 1-D compound type.
+  auto FieldIdx = getGEPIndex(CurGEP);
+  auto RelOffset = getByteOffsetRelative(CurGEP->getSourceElementType(), FieldIdx);
+  //errs() << "CurGEP = " << *CurGEP << " Field Idx = " <<  FieldIdx << " RelOffset = " << RelOffset << "\n";
+  CurOffset += RelOffset;
+
+  if (!isa<GetElementPtrInst>(CurGEP->getNextNode()))
+    return CurOffset;
+
+  return getByteOffsetAbsolute(cast<GetElementPtrInst>(CurGEP->getNextNode()), CurOffset);
+}
+
+bool CacheUsageInfo::analyze() {
+  using BasePtrType = std::pair<const llvm::Value *, unsigned>;
+
+  // Each entry is unique and non-aliasing
+  DenseSet<std::pair<BasePtrType, unsigned>> MemoryCacheLineId;
+
+  // Collect Alloca variable with content as pointer type.
+  // We don't track explicitely stack allocated memory for now.
+  for (const auto & I : F->getEntryBlock()) {
+    if (auto * Alloca = dyn_cast<AllocaInst>(&I)) {
+      auto * Pointee = Alloca->getAllocatedType();
+      if (!Pointee->isPointerTy()) continue;
+      if (Pointee->getPointerElementType()->isSingleValueType()) continue;
+      PtrAllocas.push_back(Alloca);
+    }
+  }
+
+  // Check the presence of a loop
+  if (!LI.empty())
+    LoopExists = true;
+
+  for (const auto & Alloca : PtrAllocas) {
+    // Each Memory Object having compound data type like struct, its size
+    // can be represented in terms of number of cache lines i.e suppose all 
+    // fields of the compound types are accessed, then how many cache lines
+    // taken to keep the data in cache.
+    //
+    // Suppose field A to be accessed is already present in cache then it is
+    // cache hit. This can happen if field B 
+    // * is accessed previously.
+    // * it belongs to the same cache line as A.
+    // * cache line is not replaced. (XXX We assume this condition met always).
+
+    for (auto * U : Alloca->users()) {
+      if (!isa<LoadInst>(U)) continue;
+      const Instruction * BasePtr = cast<LoadInst>(U);
+      BasePtrType Key = std::make_pair(Alloca, 0);
+
+      // Check whether ptr is double pointer, then update key and baseptr
+      // XXX Bit hacky, any elegant way?
+      if (Alloca->getAllocatedType()->getPointerElementType()->isPointerTy()) {
+        Key = std::make_pair(Alloca, getGEPIndex(cast<GetElementPtrInst>(BasePtr->getNextNode())));
+        BasePtr =  BasePtr->getNextNode()->getNextNode();
+      }
+
+      LLVM_DEBUG(dbgs() << "\nKey = " << *Key.first << " " << Key.second << "\n");
+      LLVM_DEBUG(dbgs() << "Use = " << *U << "\n");
+      if (!isa<GetElementPtrInst>(BasePtr->getNextNode())) continue;
+
+      unsigned AbsoluteOffset = getByteOffsetAbsolute(cast<GetElementPtrInst>(BasePtr->getNextNode()), 0 /*Base Address idx */);
+      //errs() << "Absolute Offset = " << AbsoluteOffset 
+      //  << " Cacheline " << AbsoluteOffset/CacheLineSize << "\n";
+      MemoryCacheLineId.insert(std::make_pair(Key, AbsoluteOffset/CacheLineSize));
+    }
+  }
+
+  // total number of cache lines tracked.
+  NumOfCacheLines = MemoryCacheLineId.size();
+
+ return false;
+}
+
+} // tas namespace
