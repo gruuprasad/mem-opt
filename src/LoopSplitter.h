a0371643a81e77c65cebff3b359bbf2e25c3f9f7
guruhegde <guruhegde1308@gmail.com>
2019-10-07 10:10:42 +0200

    Clean up LoopSplitter class

--- src/LoopSplitter.h
+++ src/LoopSplitter.h
@@ -22,21 +22,12 @@ llvm::AllocaInst * getLoopIndexVar(llvm::Loop * L);
 class LoopSplitter {
   llvm::Function * F;
   llvm::LoopInfo * LI;
-  llvm::SmallVector<llvm::Value *, 4> AnnotatedVariables;
-  llvm::SmallVector<llvm::Instruction *, 4> AnnotatedVariableDefPoints;
-  llvm::SmallVector<llvm::Value *, 4> PrefetchAddresses;
-  llvm::Value * InductionVariable;
 
   public:
   LoopSplitter(llvm::Function * F_, llvm::LoopInfo * LI_)
     : F(F_), LI(LI_) {}
 
   bool run();
-  unsigned getAnnotatedVarCount() { return AnnotatedVariables.size(); }
-  void addEmptyLoop(llvm::BasicBlock * InsertBefore);
-  void findVariableUsePoints();
-  void splitLoop(llvm::Loop * L0);
-  void fixValueDependenceBetWeenLoops(TASForLoop * NewLoop, llvm::Value * OldIndex);
 };
 
 } // tas namespace
