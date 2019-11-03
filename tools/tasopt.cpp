52b5bb8385b820d9487fdf35a0249d213da406a7
guruhegde <guruhegde1308@gmail.com>
2019-11-03 17:35:29 +0100

    Enable LoopSplitting

--- tools/tasopt.cpp
+++ tools/tasopt.cpp
@@ -99,27 +99,17 @@ int main(int argc, char * argv[]) {
     }
 
     if (FnStr.second.compare("tas_batch_maker") == 0) {
-      errs() << "Non batch function " << FnStr.first->getName() << "\n";
-      errs() << "Running block predication\n";
       tas::BlockPredication BP (FnStr.first);
       auto res = BP.run();
       // Make Batch version
-      errs() << "Running batching\n";
       tas::BatchMaker BM(FnStr.first);
       auto BatchFunc = BM.run();
-      errs() << BatchFunc->getName() << "\n";
+      // Loop Splitting
+      DominatorTree DT(*BatchFunc);
+      LoopInfo LI(DT);
+      tas::LoopSplitter LS(BatchFunc, &LI);
+      res = LS.run();
     }
-
-    /*
-
-    writeToAsmFile(*M);
-    // Loop Splitting
-    DominatorTree DT(*BatchFunc);
-    LoopInfo LI(DT);
-    tas::LoopSplitter LS(BatchFunc, &LI);
-    res = LS.run();
-    errs() << res << "\n";
-    */
   }
 
   writeToAsmFile(*M);
